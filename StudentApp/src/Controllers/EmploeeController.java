165170e7ab5430ec1dc60dde786cfb3232c9a971
Alexey Neroda <neroda.all@mail.ru>
2023-07-09 11:21:57 +0300

    seminar4 done

---
+++ StudentApp/src/Controllers/EmploeeController.java
@@ -0,0 +1,19 @@
+package Controllers;
+
+import Domen.Emploee;
+import Services.EmploeeService;
+//import Services.StudentService;
+
+public class EmploeeController implements iPersonController<Emploee> {
+    private final EmploeeService empService = new EmploeeService();
+
+    @Override
+    public void create(String firstName, int age) {
+        empService.create(firstName,age);
+    }
+
+    public static <T extends Emploee> void paySalary(T person)
+    {
+        System.out.println(person.getName()+" выплачена зарплата 10000р. ");
+    }
+}
